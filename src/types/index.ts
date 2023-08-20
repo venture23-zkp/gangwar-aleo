f893bed5b584928df20889f2840bad91a28b0fef
Prajwol Gyawali <mail.prajwolgyawali@gmail.com>
2023-08-20 19:06:13 +0545

    Remove weaponNFT

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,6 @@ export * from "./common";
 export * from "./error";
 export * from "./status-codes";
 export * from "./nft";
-export * from "./weaponNFT";
 export * from "./gangwar";
 export * from "./simulationEngine";
 export * from "./dsa";
